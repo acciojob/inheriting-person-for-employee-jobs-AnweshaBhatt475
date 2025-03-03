1f9547e695908751b65122700dc1599407479a18
AnweshaBhatt475 <143898640+AnweshaBhatt475@users.noreply.github.com>
2025-03-03 23:29:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 class Person {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-  greet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
-  }
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
 }
 
 class Employee extends Person {
-  constructor(name, age, jobTitle) {
-    super(name, age); // Calling the parent class constructor
-    this.jobTitle = jobTitle;
-  }
+    constructor(name, age, jobTitle) {
+        super(name, age); // Calling the parent class constructor
+        this.jobTitle = jobTitle;
+    }
 
-  jobGreet() {
-    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-  }
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
 }
 
 // Do not change code below this line
@@ -26,6 +26,6 @@ window.Employee = Employee;
 
 // Example usage
 let person = new Person("Alice", 25);
-person.greet(); 
+person.greet();
 let employee = new Employee("Bob", 30, "Manager");
-employee.jobGreet(); 
+employee.jobGreet();
